3de29e09e8e79960eb2bfdab2215765c2c9b9df4
Gaëtan <gaetan.hrlt@gmail.com>
2022-04-04 11:59:42 +0200

    Added cmd args + basic err handler

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import requests
 import time
 
+token = None
 """
 Obtain like this:
 	- Open and login into discord in your browser
@@ -13,6 +14,7 @@ Obtain like this:
 		/!\ Be fast, it will disappear
 """
 
+channel_id = None
 """
 Obtain like this:
 	- Activate Developer Options in your parameters
@@ -20,8 +22,43 @@ Obtain like this:
 	- Click "Copy ID"
 """
 
-token = input("What's your Discord account token? ")
-channel_id = input("What's the channel id? ")
+if __name__ == "__main__":
+	import sys
+
+	args = sys.argv[1:]
+	if args:
+		if "-h" in args or "--help" in args:
+			print("")
+			print("WritingForever for Discord")
+			print("by GaHrlt - https://github.com/ghrlt/writing-forever-discord")
+			print("")
+			print("Usage:")
+			print("\t-h, --help\t\tDisplay this message")
+			print("\t-t, --token[=<token>]\tProvide your Discord Account Token")
+			print("\t-i, --id[=<channel_id>]\tProvide the channel ID to work in")
+			sys.exit()
+
+		else:
+			_args = []
+			for arg in args:
+				_args += arg.split('=')
+
+			args = _args
+
+			if "-t" in args:
+				token = args[args.index("-t")+1]
+			elif "--token" in args:
+				token = args[args.index("-t")+1]
+
+			if "-i" in args:
+				channel_id = args[args.index("-i")+1]
+			elif "--channel_id" in args:
+				channel_id = args[args.index("--channel_id")+1]
+
+	if not token:
+		token = input("What's your Discord account token? ")
+	if not channel_id:
+		channel_id = input("What's the channel id? ")
 
 
 h = {
@@ -31,5 +68,8 @@ h = {
 
 while True:
 	r = requests.post(f"https://discord.com/api/v9/channels/{channel_id}/typing", headers=h)
-	print(r, r.content)
-	time.sleep(2.5)
+	if r.status_code == 200:
+		time.sleep(2.5)
+	else:
+		print("Failed.. Retrying in 5m -", r.text)
+		time.sleep(5*60)
